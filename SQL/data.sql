23ffb8d4bd6e1db85c730606f874258f68f3aeb2
lennertamas <tamas.lenner@gmail.com>
2024-10-14 15:00:09 +0200

    updated

--- SQL/data.sql
+++ SQL/data.sql
@@ -15,7 +15,6 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    total_amount DECIMAL(10, 2),
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
